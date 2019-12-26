4a35a2f76e227d613766acd946ed65c0872fbe6e
Nazam Hussain <nhussain2@plus.net>
2019-12-26 22:55:37 +0000

    Added Step 4 - in Git Demo

--- git-demo/background.sh
+++ git-demo/background.sh
@@ -4,4 +4,5 @@ docker build --tag gitserver /var/tmp
 docker run --name gitserver -dit --rm gitserver
 cp /var/tmp/id_rsa /root/.ssh/
 cp /var/tmp/id_rsa.pub /root/.ssh/
+sleep 30
 ssh -o 'StrictHostKeyChecking no' -f root@gitserver ls
