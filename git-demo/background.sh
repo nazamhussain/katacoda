e6dda3d638c95a884d3408b8b8c4914469926aba
Nazam Hussain <nhussain2@plus.net>
2019-12-26 22:53:21 +0000

    Added Step 4 - in Git Demo

--- git-demo/background.sh
+++ git-demo/background.sh
@@ -2,3 +2,6 @@
 touch data.txt
 docker build --tag gitserver /var/tmp
 docker run --name gitserver -dit --rm gitserver
+cp /var/tmp/id_rsa /root/.ssh/
+cp /var/tmp/id_rsa.pub /root/.ssh/
+ssh -o 'StrictHostKeyChecking no' -f root@gitserver ls
--- git-demo/foreground.sh
+++ git-demo/foreground.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 export PS1="\[\e[32m\]\u\[\e[m\]\[\e[32m\]@\[\e[m\]\[\e[32m\]\h\[\e[m\]:\[\e[34m\]\W\[\e[m\]\\$ "
 echo "172.18.0.2 gitserver" >> /etc/hosts
-sleep 2
-cp /var/tmp/id_rsa /root/.ssh/
-cp /var/tmp/id_rsa.pub /root/.ssh/
-ssh -o 'StrictHostKeyChecking no' -f root@gitserver ls
+#sleep 2
+#cp /var/tmp/id_rsa /root/.ssh/
+#cp /var/tmp/id_rsa.pub /root/.ssh/
+#ssh -o 'StrictHostKeyChecking no' -f root@gitserver ls
 clear
