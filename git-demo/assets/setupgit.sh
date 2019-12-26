6a15db97f297d8b677b8197e7d39d1ebe8098051
Nazam Hussain <nhussain2@plus.net>
2019-12-26 17:57:31 +0000

    Added Step 4 - in Git Demo

--- git-demo/assets/setupgit.sh
+++ git-demo/assets/setupgit.sh
@@ -5,4 +5,10 @@ echo this is a dummy readme file > /root/git/remoterepo/readme.txt
 echo this is a dummy script file > /root/git/remoterepo/script.txt
 echo this is a dummy variables file > /root/git/remoterepo/variables.txt
 echo this is a dummy config file > /root/git/remoterepo/config.txt
+git config --global user.name "Bugs Bunny"
+git config --global user.email "bugs@bunny.com"
 git init /root/git/remoterepo
+cd /root/git/remoterepo
+git add .
+git commit -m "Initial Commit"
+cd /
