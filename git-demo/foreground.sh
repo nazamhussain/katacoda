263a1c60d2931b43e7ff64122e1711a4d04ee954
Nazam Hussain <nhussain2@plus.net>
2019-12-26 21:59:30 +0000

    Added Step 4 - in Git Demo

--- git-demo/foreground.sh
+++ git-demo/foreground.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 export PS1="\[\e[32m\]\u\[\e[m\]\[\e[32m\]@\[\e[m\]\[\e[32m\]\h\[\e[m\]:\[\e[34m\]\W\[\e[m\]\\$ "
 echo "172.18.0.2 gitserver" >> /etc/hosts
+cp /var/tmp/id_rsa /root/.ssh/
+cp /var/tmp/id_rsa.pub /root/.ssh/
 clear
