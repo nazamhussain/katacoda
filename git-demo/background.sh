86fb066cb369698454ab4344c44b6de7ebd5490e
Nazam Hussain <nhussain2@plus.net>
2019-12-26 23:00:08 +0000

    Added Step 4 - in Git Demo

--- git-demo/background.sh
+++ git-demo/background.sh
@@ -2,7 +2,3 @@
 touch data.txt
 docker build --tag gitserver /var/tmp
 docker run --name gitserver -dit --rm gitserver
-cp /var/tmp/id_rsa /root/.ssh/
-cp /var/tmp/id_rsa.pub /root/.ssh/
-sleep 30
-ssh -o 'StrictHostKeyChecking no' -f root@gitserver ls
