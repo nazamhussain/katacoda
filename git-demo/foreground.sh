573d7a7d139a273e03a27c6fa9d2607ee3f9069d
Nazam Hussain <nhussain2@plus.net>
2019-12-26 23:12:05 +0000

    Added Step 4 - in Git Demo

--- git-demo/foreground.sh
+++ git-demo/foreground.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 export PS1="\[\e[32m\]\u\[\e[m\]\[\e[32m\]@\[\e[m\]\[\e[32m\]\h\[\e[m\]:\[\e[34m\]\W\[\e[m\]\\$ "
 echo "172.18.0.2 gitserver" >> /etc/hosts
-while ! ping -c 1 -n -w 1 172.18.0.2 &> /dev/null; do printf "%c" "."; done
+while ! ping -c 1 -n -w 1 172.18.0.2 &> /dev/null; do sleep 1; done
 sleep 10
 cp /var/tmp/id_rsa /root/.ssh/
 cp /var/tmp/id_rsa.pub /root/.ssh/
