c17dce00e543f36fdc4596d97fdd11786ebe9318
Nazam Hussain <nhussain2@plus.net>
2019-12-26 23:14:56 +0000

    Added Step 4 - in Git Demo

--- git-demo/foreground.sh
+++ git-demo/foreground.sh
@@ -2,8 +2,9 @@
 export PS1="\[\e[32m\]\u\[\e[m\]\[\e[32m\]@\[\e[m\]\[\e[32m\]\h\[\e[m\]:\[\e[34m\]\W\[\e[m\]\\$ "
 echo "172.18.0.2 gitserver" >> /etc/hosts
 while ! ping -c 1 -n -w 1 172.18.0.2 &> /dev/null; do sleep 1; done
-sleep 30
+sleep 20
 cp /var/tmp/id_rsa /root/.ssh/
 cp /var/tmp/id_rsa.pub /root/.ssh/
 ssh -o 'StrictHostKeyChecking no' -f root@gitserver ls
+clear
 clear
