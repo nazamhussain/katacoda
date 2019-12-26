648e339f91d5b57cd6fe14166b3ee597a5887f7d
Nazam Hussain <nhussain2@plus.net>
2019-12-26 18:52:00 +0000

    Added Step 4 - in Git Demo

--- git-demo/assets/setupgit.sh
+++ git-demo/assets/setupgit.sh
@@ -2,9 +2,6 @@
 mkdir -p /root/git/remoterepo/data
 echo this is a dummy data file > /root/git/remoterepo/data/data.txt
 echo this is a dummy readme file > /root/git/remoterepo/readme.txt
-echo this is a dummy script file > /root/git/remoterepo/script.txt
-echo this is a dummy variables file > /root/git/remoterepo/variables.txt
-echo this is a dummy config file > /root/git/remoterepo/config.txt
 git config --global user.name "Bugs Bunny"
 git config --global user.email "bugs@bunny.com"
 git init /root/git/remoterepo
@@ -12,10 +9,16 @@ cd /root/git/remoterepo
 git add .
 git commit -m "Initial Commit"
 sleep 1
+echo this is a dummy script file > /root/git/remoterepo/script.txt
+git add .
 git commit -m "Another Commit"
 sleep 1
+echo this is a dummy variables file > /root/git/remoterepo/variables.txt
+git add .
 git commit -m "Yet Another Commit"
 sleep 1
+echo this is a dummy config file > /root/git/remoterepo/config.txt
+git add .
 git commit -m "Committed Again!"
 cd /
 while true; do sleep 1; done
