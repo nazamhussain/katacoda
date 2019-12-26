9ec83058c390ce9e77deebfde0b0e7423df424fb
Nazam Hussain <nhussain2@plus.net>
2019-12-26 18:06:58 +0000

    Added Step 4 - in Git Demo

--- git-demo/background.sh
+++ git-demo/background.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 touch data.txt
 docker build --tag gitserver /var/tmp
-docker run --name gitserver -dit --rm --volume /var/tmp:/var/tmp gitserver
+docker run --name gitserver -dit --rm gitserver
