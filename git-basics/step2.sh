95689d582424c29fab74139734cf17bfc966c918
Nazam Hussain <nhussain2@plus.net>
2020-01-08 12:53:58 +0000

    Committed at 12:53:58 on 08/01/2020

---
+++ git-basics/step2.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo `date` > /root/localrepo/timestamps.txt
+echo `date` > /root/localrepo/picture.jpg
+echo `date` > /root/localrepo/document.doc
+echo `date` > /root/localrepo/portable.pdf
+echo `date` > /root/localrepo/application.exe
